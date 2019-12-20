4d1444ead0749d6aa31e0d2e2ddd64da9f4352ba
Andre Cesar Gomes <andre270492@gmail.com>
2019-12-20 20:06:50 -0200

    Add unique users validation for inscription

--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -3,12 +3,18 @@
 class Inscription < ApplicationRecord
   belongs_to :user
   belongs_to :job
-  validates :user, uniqueness: true
+  validate :job_has_uniq_users, on: :create
   validate :user_has_a_profile
 
   def user_has_a_profile
     return if user.profile.present? && !user.profile.pending?
 
     errors.add(:user, 'precisa preencher o perfil para poder se candidatar')
+  end
+
+  def job_has_uniq_users
+    return if job.users.blank? || !job.users.include?(user)
+
+    errors.add(:user, 'já inscrito')
   end
 end
--- spec/models/inscription_spec.rb
+++ spec/models/inscription_spec.rb
@@ -45,4 +45,73 @@ describe Inscription do
         eq(['Candidato precisa preencher o perfil para poder se candidatar'])
     end
   end
+
+  describe '.job_has_uniq_users' do
+    it 'and is the first inscription at the job' do
+      user = create(:user)
+      create(:profile, user: user, status: 'complete')
+      inscription = build(:inscription, user: user)
+
+      expect(inscription.valid?).to be_truthy
+    end
+
+    it 'and its already inscribed to the job' do
+      user = create(:user)
+      create(:profile, user: user, status: 'complete')
+      job = create(:job)
+      create(:inscription, user: user, job: job)
+      inscription = build(:inscription, user: user, job: job)
+
+      job.reload
+      inscription.valid?
+
+      expect(inscription.errors.full_messages).to eq(['Candidato já inscrito'])
+    end
+
+    it 'and has another candidate already applied to the job' do
+      user = create(:user)
+      another_user = create(:user, email: 'another@email.com')
+      create(:profile, user: user, status: 'complete')
+      create(:profile, user: another_user, status: 'complete')
+      job = create(:job)
+      create(:inscription, user: user, job: job)
+      inscription = build(:inscription, user: another_user, job: job)
+
+      job.reload
+      inscription.valid?
+
+      expect(inscription.errors).to be_empty
+    end
+
+    it 'and candidate have already applied for other job of the same hunter' do
+      user = create(:user)
+      create(:profile, user: user, status: 'complete')
+      hunter = create(:hunter)
+      job = create(:job, hunter: hunter)
+      another_job = create(:job, title: 'Other Job', hunter: hunter)
+      create(:inscription, user: user, job: another_job)
+      inscription = build(:inscription, user: user, job: job)
+
+      job.reload
+      inscription.valid?
+
+      expect(inscription.errors).to be_empty
+    end
+
+    it 'and candidate have already applied for job of different hunter' do
+      user = create(:user)
+      create(:profile, user: user, status: 'complete')
+      hunter = create(:hunter)
+      another_hunter = create(:hunter, email: 'another@hunter.com')
+      job = create(:job, hunter: hunter)
+      another_job = create(:job, title: 'Other Job', hunter: another_hunter)
+      create(:inscription, user: user, job: another_job)
+      inscription = build(:inscription, user: user, job: job)
+
+      job.reload
+      inscription.valid?
+
+      expect(inscription.errors).to be_empty
+    end
+  end
 end
